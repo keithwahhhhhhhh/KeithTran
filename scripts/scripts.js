a2158e2b27abb0ef8d84197874a47130c2ffcd7d
Keith Tran <60492188+keithwahhhhhhhh@users.noreply.github.com>
2021-10-12 22:06:23 -0700

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,15 @@ $(function () {
     document.title = "Mental Health Quiz"
 
   })
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const email = urlParams.get('email');
+  const pickanum = urlParams.get('pickanum');
+  const colors = urlParams.getAll('colors')
+  const writestuff = urlParams.get('writestuff');
+  var output = "Your email is: " + email + " <br> The number you picked is: " + pickanum + "<br> the colors you picked were: " + colors + "<br> and you wrote the following: <br>" + writestuff;
+
+  jQuery(document).ready(function() {
+    $("#results").html(output); 
+  });
